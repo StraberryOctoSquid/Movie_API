991e57e4c359bedd39050d4ff6e26b4f496b07c7
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-06-14 15:52:21 -0500

    create top movies list

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 const express = require("express");
 const app = express();
+
+let topMovies = [
+
+    {
+        title: 'The Wizard of Oz',
+        director: 'Victor Flemming',
+    },
+    {
+        title: 'Papillon',
+        director: 'Franklin J. Shaffner',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Back to the Future',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    {
+        title: 'Jaws',
+        director: 'Steven Spielberg',
+    },
+    
+]
