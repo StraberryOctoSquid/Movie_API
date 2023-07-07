621978cd47579edc64fe4fbe9497e7b562c22934
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-07 09:33:58 -0500

    grant access to specific domains

--- index.js
+++ index.js
@@ -14,11 +14,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // require cors
+// Delete this line?
+const cors = require('cors');
+
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-// Delete this line?
-// const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = "The CORS policy for this application doesn't allow access from origin" + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth.js')(app);
 
