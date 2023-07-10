c1d68f8bf74e648da728c49a8a27564dc2941a8a
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-10 14:15:07 -0500

    Create imp_exp_code.js
    copy and paste reference for creating mongodb export .json files. Code for exporting them to atlas.

---
+++ imp_exp_code.js
@@ -0,0 +1,5 @@
+mongoexport -d cdDB -c  users -o  users.json
+
+
+mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection movies --type json --file movies.json
+mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection users --type json --file users.json
