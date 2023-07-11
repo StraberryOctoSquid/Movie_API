8c90467984559edf4d430f9bb45d6914182a953c
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-11 06:51:19 -0500

    comment out import export code

--- imp_exp_code.js
+++ imp_exp_code.js
@@ -1,8 +1,8 @@
-mongoexport -d cdDB -c  users -o  users.json
+// mongoexport -d cdDB -c  users -o  users.json
 
 
-mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection movies --type json --file movies.json
-mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection users --type json --file users.json
+// mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection movies --type json --file movies.json
+// mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection users --type json --file users.json
 
 
-mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/?retryWrites=true&w=majority
