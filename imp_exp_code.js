e226bd9fa335e955422c4215ede8266790422651
StraberryOctoSquid <126728208+StraberryOctoSquid@users.noreply.github.com>
2023-07-19 13:47:40 -0500

    Delete imp_exp_code.js

--- imp_exp_code.js
+++
@@ -1,8 +0,0 @@
-// mongoexport -d cdDB -c  users -o  users.json
-
-
-// mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/test --collection movies --type json --file movies.json
-// mongoimport --uri mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/myFlixDB --collection users --type json --file users.json
-
-
-// mongodb+srv://johntest:1234@filmdb.kwgqh67.mongodb.net/?retryWrites=true&w=majority
